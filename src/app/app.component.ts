b9b15708ac01554348721eebdc2eb76855e49183
Drew Morehouse <drew_morehouse@solutiaconsulting.com>
2024-06-26 16:57:42 -0500

    Working with project components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { ProjectItemComponent } from './components/project-item/project-item.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, ProjectItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
