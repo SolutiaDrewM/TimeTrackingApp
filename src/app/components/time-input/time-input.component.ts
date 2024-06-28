629f07bff7e5f99f2c79e64d8c5add2e2b5f3f3f
Drew Morehouse <drew_morehouse@solutiaconsulting.com>
2024-06-28 15:48:43 -0500

    Making the input form a reactive form for better validation

--- src/app/components/time-input/time-input.component.ts
+++ src/app/components/time-input/time-input.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 import { MatFormField } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
 import { Project } from '../../interfaces/project';
 import { ProjectService } from '../../services/project.service';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -12,27 +10,39 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [
     FormsModule, 
+    ReactiveFormsModule,
     CommonModule,
-    MatFormField, 
-    MatInputModule, 
-    MatSelectModule
+    MatFormField,
   ],
   templateUrl: './time-input.component.html',
   styleUrl: './time-input.component.css'
 })
 export class TimeInputComponent {
+
+  //Empty form with no controls to please the compiler
+  taskForm: FormGroup = new FormGroup<any>({});
+
   titleInputValue: string = '';
   projectTitles: string[] = [];
   filteredTitles: string[] = [];
   dropdownVisible: boolean = false;
 
-  constructor(private projectService: ProjectService) {}
+  projectTitle: string = '';
+
+
+  constructor(private projectService: ProjectService, private fb: FormBuilder) {}
 
   ngOnInit() {
     this.projectTitles = this.getProjectTitles();
+    this.filteredTitles = this.projectTitles;
+    this.taskForm = this.fb.group({
+      title: ['', Validators.required],
+      task: ['', Validators.required],
+      hours: ['', [Validators.required, Validators.min(0)]]
+    })
   }
 
-  getProjectTitles() {
+  getProjectTitles(): string[] {
     return this.projectService.getProjects().map(project => project.title);
 
     //For later use of an observable
@@ -41,6 +51,15 @@ export class TimeInputComponent {
     }); */
   }
 
+  onSubmit(): void {
+    console.log('Form Data: ', this.taskForm.value);
+  }
+
+  getTitleInput() {
+    
+  }
+
+  //Functions for the dropdown menu
   showSuggestions(): void {
     if (this.titleInputValue) {
       this.filteredTitles = this.projectTitles.filter(title =>
@@ -48,7 +67,6 @@ export class TimeInputComponent {
       );
       this.dropdownVisible = true;
     } else {
-      this.filteredTitles = [];
       this.dropdownVisible = false;
     }
   }
