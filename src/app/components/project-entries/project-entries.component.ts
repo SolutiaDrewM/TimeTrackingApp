5be4ae1588b27f47357c6c4d77fe99171059aea1
Drew Morehouse <drew_morehouse@solutiaconsulting.com>
2024-07-01 13:06:31 -0500

    Selected project communicates with the page

--- src/app/components/project-entries/project-entries.component.ts
+++ src/app/components/project-entries/project-entries.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { Project } from '../../interfaces/project';
 
@@ -10,6 +10,19 @@ import { Project } from '../../interfaces/project';
   styleUrl: './project-entries.component.css'
 })
 export class ProjectEntriesComponent {
+  @Input() projectTitle: string | null | undefined;
   //selectedProject: Project;
   //timeEntryData: TimeEntry[];
+
+  ngOnChanges(): void {
+    this.renderEntries
+  }
+
+  renderEntries(): void {
+    //Render entries based on projectTitle
+
+    // if title is undefined or null, render the blank gray
+
+    // else render the title and a table of entries
+  }
 }
--- src/app/components/time-entry/time-entry.component.ts
+++ src/app/components/time-entry/time-entry.component.ts
@@ -4,13 +4,19 @@ import { CreateProjectComponent } from "../create-project/create-project.compone
 import { ProjectEntriesComponent } from "../project-entries/project-entries.component";
 import { Project } from '../../interfaces/project';
 import { SelectedProjectService } from '../../services/selected-project.service';
+import { NgIf } from '@angular/common';
 
 @Component({
     selector: 'app-time-entry',
     standalone: true,
     templateUrl: './time-entry.component.html',
     styleUrl: './time-entry.component.css',
-    imports: [ProjectListComponent, CreateProjectComponent, ProjectEntriesComponent]
+    imports: [
+        ProjectListComponent,
+        CreateProjectComponent,
+        ProjectEntriesComponent,
+        NgIf
+    ]
 })
 export class TimeEntryComponent implements OnInit {
     selectedProject: string | null = null;
